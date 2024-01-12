ea6d48e55fa383b3c0ca8883b834537ec1453c5e
Eduardo IT <eiglesias@platkmframework.org>
2024-01-12 15:55:28 +0100

    update test APIs and names

--- src/main/java/com/example/controller/PersonController.java
+++ src/main/java/com/example/controller/PersonController.java
@@ -6,7 +6,7 @@ import org.platkmframework.annotation.HttpRequestMethod;
 import org.platkmframework.annotation.RequestBody;
 import org.platkmframework.annotation.RequestParam;
 
-@Api(path = "/persona", description = "Gestión de personas")
+@Api(path = "/person", description = "Example APIs")
 public class PersonController {
 
 	public PersonController() {
@@ -14,22 +14,22 @@ public class PersonController {
 	}
 	
 	@ClassMethod(name="/{id}",   method = HttpRequestMethod.GET)
-	public Persona obtenerNombrePersona(@RequestParam(name = "id") Long id) {
-		Persona persona = new Persona();
-		persona.setApellidos("Appellido 1");
-		persona.setNombre("Nombre 1");
+	public Person getPersonById(@RequestParam(name = "id") Long id) {
+		Person person = new Person();
+		person.setLastname("Last Name " + id);
+		person.setName("Name  " + id);
 				
-		return persona;
+		return person;
 	}
 	
-	@ClassMethod(name = "/cambiar/{nombre}", method = HttpRequestMethod.PUT)
-	public String cambiarNombrePersona(@RequestParam(name = "nombre") String nombre ) {
-		return "Nuevo Nombre: " + nombre;
+	@ClassMethod(name = "/change/{name}", method = HttpRequestMethod.PUT)
+	public String changePersonName(@RequestParam(name = "name") String name ) {
+		return "new Name: " + name;
 	}
 	
 	@ClassMethod(name = "/create", method = HttpRequestMethod.POST)
-	public void crearPersona(@RequestBody() Persona persona ) {
-		System.out.print(persona.getNombre() + " - " + persona.getApellidos());
+	public void crearPersona(@RequestBody() Person person ) {
+		System.out.print(person.getName() + " - " + person.getLastname());
 	}
 
 }
--- src/main/java/com/example/start/RunApplication.java
+++ src/main/java/com/example/start/RunApplication.java
@@ -2,11 +2,16 @@ package com.example.start;
 
 import org.platkmframework.boot.jpa.server.runner.PlatkmFrameworkApplication;
 import org.platkmframework.content.project.ProjectContent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 public class RunApplication {
+	private static Logger logger = LoggerFactory.getLogger(RunApplication.class);
 
 	public static void main(String[] args) {
-		
+		logger.info("Starting..");
 		ProjectContent.instance().projectName("Example"). //change it with your project name
 								  server("localhost"). // change it with your server name or ip
 								  servletPath("/example"). // path for end point
